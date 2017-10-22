884216facb898171706f4653576472a033243fc3
Pierre Zemb <contact@pierrezemb.fr>
2017-10-22 16:23:41 +0200

    doc(godoc): updating

--- doc.go
+++ doc.go
@@ -4,21 +4,33 @@ Example:
   package main
 
   import (
-    warp "github.com/PierreZ/Warp10Exporter"
+  	"time"
+
+  	warp "github.com/PierreZ/Warp10Exporter"
   )
 
   func main() {
 
-    gts := warp.NewGTS("metrics.test").WithLabels(warp.Labels{
-      "ip": "1.2.3.4",
-    }).AddDatapoint(time.Now(), "42")
-    warp.PushGTS(gts, "http://localhost:8080", "WRITE_TOKEN")
+  	gts := warp.NewGTS("metrics.test").WithLabels(warp.Labels{
+  		"ip": "1.2.3.4",
+  	}).AddDatapoint(time.Now(), "42")
+  	// Not checking the error
+  	gts.Push("http://localhost:8080", "WRITE_TOKEN")
 
-    // You can also create batchs
-    batch := warp.NewBatch()
-    batch.Register(gts)
+  	// You can also create batchs
+  	batch := warp.NewBatch()
+  	batch.Register(gts)
+  	gts.AddDatapoint(ts, 42)
 
-    warp.PushBatch(batch, "http://localhost:8080", "WRITE_TOKEN")
+  	err := batch.Push("http://localhost:8080", "WRITE_TOKEN")
+  	if err != nil {
+  		// You can also write metrics to a file, to use
+  		// https://github.com/ovh/beamium for example
+  		err = batch.FlushOnDisk("/opt/beamium/sink")
+  		if err != nil {
+  			panic(err)
+  		}
+  	}
   }
 
 For a full guide visit https://github.com/PierreZ/Warp10Exporter
